e8c27d6f5f3ec6fd871cb20c5fa88c031f2833be
garotogordo <garotogordo@icloud.com>
2022-03-31 11:49:09 -0300

    voltando a pagina de erro antiga

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,31 +1,19 @@
-import type { FC } from "react";
-import { useEffect, useState } from "react";
-import { useRouter } from "next/router";
+import { NextPage } from 'next';
+import { Navbar } from '@components/Navbar';
+import { Header } from '@components/Header';
 
-interface ICustom404 {
-  statusCode: number;
-}
-
-export const Custom404: FC<ICustom404> = function ({ statusCode = 404 }) {
-  const [leftSecond, setLeftSecond] = useState<number>(5);
-
-  useEffect(() => {
-    setInterval(
-      () => (leftSecond >= 0 ? setLeftSecond(leftSecond - 1) : null),
-      900
+const Custom404Page:NextPage = () => {
+    return (
+     <div className="">
+         <div>
+            <Navbar/>
+         </div>
+         <div className='flex h-screen items-center justify-center'>
+            <p>Not found.. im still making this page lol.</p>
+         </div>
+        <Header title='Not Found!'/>
+     </div>
     );
-    setTimeout(() => {
-      window.location.assign("/");
-    }, 5000);
-  });
-  return (
-    <div className="bg-Bgnavbar w-full h-full absolute flex items-center justify-center text-white">
-      <div className="items-center flex flex-col space-y-7">
-        <a className="text-xl text-purple-300 font-extrabold md:text-7xl text-center">
-          <span className="text-7xl">{statusCode} - Page Not Found</span>
-        </a>
-        <p className="text-gray-400">Redirecionando em {leftSecond}</p>
-      </div>
-    </div>
-  );
 };
+
+export default Custom404Page;
