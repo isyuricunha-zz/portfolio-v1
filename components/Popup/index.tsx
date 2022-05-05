774b6ad03e2524397539872a5d5fc8adbd45f871
Yuri Cunha <isyuricunha@icloud.com>
2022-05-05 20:20:58 -0300

    alterando link

--- components/Popup/index.tsx
+++ components/Popup/index.tsx
@@ -5,7 +5,7 @@ export const Popup: FC = () => {
     <div className="">
       <aside className="fixed z-50 flex items-center justify-center px-5 py-3 text-white bg-black rounded-lg bottom-4 right-4">
         <a
-          href="https://blog.isyuricunha.com"
+          href="https://www.blog.garotogordo.codes/"
           target="_blank"
           rel="noopener noreferrer"
           className="text-sm font-medium hover:opacity-75"
