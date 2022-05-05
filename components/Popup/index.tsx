deeb80b16274c1830f05c1d998331fba6fcb2f83
Yuri Cunha <isyuricunha@icloud.com>
2022-05-05 09:43:44 -0300

    atualizando links

--- components/Popup/index.tsx
+++ components/Popup/index.tsx
@@ -5,7 +5,7 @@ export const Popup: FC = () => {
     <div className="">
       <aside className="fixed z-50 flex items-center justify-center px-5 py-3 text-white bg-black rounded-lg bottom-4 right-4">
         <a
-          href="https://blog.rexulec.com"
+          href="https://blog.isyuricunha.com"
           target="_blank"
           rel="noopener noreferrer"
           className="text-sm font-medium hover:opacity-75"
