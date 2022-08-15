bde30a366e062434c654d8e5b9c150729ee9e9c5
Yuri Cunha <isyuricunha@proton.me>
2022-08-15 11:41:51 -0300

    update info's in header

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -6,37 +6,41 @@ interface IProps {
 export const Header: FC<IProps> = function ({ title }) {
   return (
     <Head>
-      <title>{title}.</title>
-      <meta name="description" content="TESTE" />
+      <title>{title} | yuricunha.</title>
+      <meta
+        name="description"
+        content="Sou um desenvolvedor Front-end com mais de 3 anos de experiência, e um estudante que passa a maior parte de seu tempo livre fazendo projetos/sites/softwares open source."
+      />
       <link rel="icon" href="/icon.png" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
-      <meta name="title" content="Yuri Cunha" />
-      <meta name="description" content="TESTE" />
+      <meta name="title" content="isyuricunha" />
+      <meta
+        name="description"
+        content="Sou um desenvolvedor Front-end com mais de 3 anos de experiência, e um estudante que passa a maior parte de seu tempo livre fazendo projetos/sites/softwares open source."
+      />
 
       <meta property="og:type" content="website" />
       <meta property="og:url" content="https://garotogordo.codes/" />
-      <meta property="og:title" content="Yuri Cunha | " />
+      <meta property="og:title" content="isyuricunha | yuri cunha" />
       <meta
         property="og:description"
-        content="Eu sou um Administrador de Banco de Dados e Programador Backend com 3 anos de experiencia.
-E um simples estudante que gasta todo o seu tempo livre fazendo projetos/sites/softwares/query's open-source."
+        content="Sou um desenvolvedor Front-end com mais de 3 anos de experiência, e um estudante que passa a maior parte de seu tempo livre fazendo projetos/sites/softwares open source."
       />
       <meta property="og:image" content="https://garotogordo.codes/icon.png" />
       <meta property="theme-color" content="#d6cdf9" />
 
-      <meta property="twitter:url" content="https://garotogordo.codes/" />
-      <meta property="twitter:title" content="https://garotogordo.codes/" />
+      <meta property="twitter:url" content="https://garotogordo.codes" />
+      <meta property="twitter:title" content="garotogordo.codes" />
       <meta
         property="twitter:description"
-        content="Eu sou um Administrador de Banco de Dados e Programador Backend com 3 anos de experiencia.
-E um simples estudante que gasta todo o seu tempo livre fazendo projetos/sites/softwares/query's open-source."
+        content="Sou um desenvolvedor Front-end com mais de 3 anos de experiência, e um estudante que passa a maior parte de seu tempo livre fazendo projetos/sites/softwares open source."
       />
       <meta
         property="twitter:image"
         content="https://garotogordo.codes/icon.png"
       />
-      <meta property="twitter:domain" content="https://garotogordo.codes/" />
+      <meta property="twitter:domain" content="garotogordo.codes" />
 
       <link rel="preconnect" href="https://fonts.googleapis.com" />
       <link rel="preconnect" href="https://fonts.gstatic.com" />
